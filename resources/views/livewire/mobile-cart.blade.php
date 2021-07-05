58304bb804c35f85135b019d5a90fa1ef29a288f
Mian Umair <mianumair1678@gmail.com>
2021-07-05 16:32:01 +0500

    Cart redirect fixed on mobile

--- resources/views/livewire/mobile-cart.blade.php
+++ resources/views/livewire/mobile-cart.blade.php
@@ -1,4 +1,4 @@
-<a class="d-table-cell handheld-toolbar-item" href="shop-cart.html">
+<a class="d-table-cell handheld-toolbar-item" href="{{ route('cart') }}">
     <span class="handheld-toolbar-icon">
         <i class="ci-cart"></i>
         <span class="badge bg-primary rounded-pill ms-1">{{ $totalItems }}</span>
