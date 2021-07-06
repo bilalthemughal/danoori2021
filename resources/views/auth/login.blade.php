f1510c22ea8adda3e76f1f0ab1770ac08747fd22
Mian Umair <mianumair1678@gmail.com>
2021-07-06 18:25:00 +0500

    remove unnecessary buttons from login and register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,10 +22,10 @@
                 <div class="card border-0 shadow">
                     <div class="card-body">
                         <h2 class="h4 mb-1">Sign in</h2>
-                        
+
                         <br>
                         <hr>
-                        
+
                         <form method="post" action="{{ route('login') }}" class="mt-4">
                             @csrf
                             <div class="input-group mb-3"><i
@@ -47,7 +47,8 @@
                             </div>
                             <div class="d-flex flex-wrap justify-content-between">
                                 <div class="form-check">
-                                    <input class="form-check-input" name="remember" {{ old('remember') ? 'checked' : ''}} type="checkbox" checked="" id="remember_me">
+                                    <input class="form-check-input" name="remember" {{ old('remember') ? 'checked' : '' }}
+                                        type="checkbox" checked="" id="remember_me">
                                     <label class="form-check-label" for="remember_me">Remember me</label>
                                 </div>
                                 <a class="nav-link-inline fs-sm" href="{{ route('password.request') }}">
@@ -71,27 +72,7 @@
 
             </section>
         </div>
-        <!-- Navigation (mobile)-->
-        <div class="row d-lg-none">
-            <div class="col-lg-8">
-                <div class="d-flex pt-4 mt-3">
-                    <div class="w-50 pe-3">
-                        <a class="btn btn-secondary d-block w-100" href="{{ route('cart') }}">
-                            <i class="ci-arrow-left mt-sm-0 me-1"></i>
-                            <span class="d-none d-sm-inline">Back to Cart</span>
-                            <span class="d-inline d-sm-none">Back</span>
-                        </a>
-                    </div>
-                    <div class="w-50 ps-2">
-                        <button class="btn btn-primary d-block w-100" type="submit">
-                            <span class="d-none d-sm-inline">Place Order</span>
-                            <span class="d-inline d-sm-none">Next</span>
-                            <i class="ci-arrow-right mt-sm-0 ms-1"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+
     </div>
 
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,16 +28,17 @@
                             @csrf
                             <div class="input-group mb-3">
                                 <i class="ci-user position-absolute top-50 translate-middle-y text-muted fs-base ms-3"></i>
-                                <input class="form-control rounded-start" value="{{ old('name') }}" name="name" type="text" placeholder="Name"
-                                    required="">
+                                <input class="form-control rounded-start" value="{{ old('name') }}" name="name"
+                                    type="text" placeholder="Name" required="">
                             </div>
                             <div class="input-group mb-3">
                                 <i class="ci-mail position-absolute top-50 translate-middle-y text-muted fs-base ms-3"></i>
-                                <input class="form-control rounded-start" value="{{ old('email') }}" name="email" type="email" placeholder="Email"
-                                    required="">
+                                <input class="form-control rounded-start" value="{{ old('email') }}" name="email"
+                                    type="email" placeholder="Email" required="">
                             </div>
                             <div class="input-group mb-3">
-                                <i class="ci-locked position-absolute top-50 translate-middle-y text-muted fs-base ms-3"></i>
+                                <i
+                                    class="ci-locked position-absolute top-50 translate-middle-y text-muted fs-base ms-3"></i>
                                 <div class="password-toggle w-100">
                                     <input class="form-control" type="password" name="password" placeholder="Password"
                                         required="">
@@ -48,10 +49,11 @@
                                 </div>
                             </div>
                             <div class="input-group mb-3">
-                                <i class="ci-locked position-absolute top-50 translate-middle-y text-muted fs-base ms-3"></i>
+                                <i
+                                    class="ci-locked position-absolute top-50 translate-middle-y text-muted fs-base ms-3"></i>
                                 <div class="password-toggle w-100">
-                                    <input class="form-control" type="password" name="password_confirmation" placeholder="Confirm Password"
-                                        required="">
+                                    <input class="form-control" type="password" name="password_confirmation"
+                                        placeholder="Confirm Password" required="">
                                     <label class="password-toggle-btn" aria-label="Show/hide password">
                                         <input class="password-toggle-check" type="checkbox">
                                         <span class="password-toggle-indicator"></span>
@@ -84,27 +86,8 @@
 
             </section>
         </div>
-        <!-- Navigation (mobile)-->
-        <div class="row d-lg-none">
-            <div class="col-lg-8">
-                <div class="d-flex pt-4 mt-3">
-                    <div class="w-50 pe-3">
-                        <a class="btn btn-secondary d-block w-100" href="{{ route('cart') }}">
-                            <i class="ci-arrow-left mt-sm-0 me-1"></i>
-                            <span class="d-none d-sm-inline">Back to Cart</span>
-                            <span class="d-inline d-sm-none">Back</span>
-                        </a>
-                    </div>
-                    <div class="w-50 ps-2">
-                        <button class="btn btn-primary d-block w-100" type="submit">
-                            <span class="d-none d-sm-inline">Place Order</span>
-                            <span class="d-inline d-sm-none">Next</span>
-                            <i class="ci-arrow-right mt-sm-0 ms-1"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+
     </div>
 
 
