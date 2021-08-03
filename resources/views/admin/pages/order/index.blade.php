a54f42318fcc7bed5776ef27d4df67b3eeb8a7ce
Mian Umair <mianumair1678@gmail.com>
2021-08-03 16:35:51 +0500

    Changed buttons in orders page

--- resources/views/admin/pages/order/index.blade.php
+++ resources/views/admin/pages/order/index.blade.php
@@ -1,9 +1,9 @@
 @extends('admin.layout')
 
 @section('extra-css')
-    <link rel="stylesheet" href="{{asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
     {{-- <link rel="stylesheet" href="{{asset('page-level/css/image-popup.css')}}"> --}}
 @stop
 
@@ -14,7 +14,9 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">{{ $status === App\Models\Order::IS_PENDING ? 'Pedning Orders' : ($status === App\Models\Order::IS_SHIPPED ? 'Completed Orders' : 'Cancelled Orders') }}</h1>
+                    <h1 class="m-0">
+                        {{ $status === App\Models\Order::IS_PENDING ? 'Pedning Orders' : ($status === App\Models\Order::IS_SHIPPED ? 'Completed Orders' : 'Cancelled Orders') }}
+                    </h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -32,8 +34,15 @@
         <div class="container-fluid">
             <div class="card">
                 <div class="card-header float-right">
-                    <a class="btn btn-success" href="{{ route('admin.order.completed') }}">Successfull Orders</a>
-                    <a class="btn btn-danger" href="{{ route('admin.order.cancelled') }}">Cancelled Orders</a>
+                    @if ($status != App\Models\Order::IS_SHIPPED)
+                        <a class="btn btn-success" href="{{ route('admin.order.completed') }}">Successfull Orders</a>
+                    @endif
+                    @if ($status != App\Models\Order::IS_PENDING)
+                        <a class="btn btn-info" href="{{ route('admin.order.index') }}">Pending Orders</a>
+                    @endif
+                    @if ($status != App\Models\Order::IS_CANCELLED)
+                        <a class="btn btn-danger" href="{{ route('admin.order.cancelled') }}">Cancelled Orders</a>
+                    @endif
                 </div>
                 <div class="card-body table-responsive">
                     <table class="table" id="orders-table">
@@ -49,22 +58,44 @@
 
 @section('extra-js')
 
-    <script src="{{asset('plugins/datatables/jquery.dataTables.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/jquery.dataTables.js') }}"></script>
 
     <script>
         $('#orders-table').DataTable({
             processing: true,
             serverSide: true,
             ajax: '{{ route('admin.order.table.data', ['id' => $status]) }}',
-            order: [ [0, 'desc'] ],
-            columns: [
-                {data: 'id', title: 'ID'},
-                {data: 'name', title: 'Name'},
-                {data: 'sub_total', title: 'Sub'},
-                {data: 'total', title: 'Total'},
-                {data: 'coupon', title: 'Coupon'},
-                {data: 'time', title: 'Placed @'},
-                {data: 'action', title: 'Action'},
+            order: [
+                [0, 'desc']
+            ],
+            columns: [{
+                    data: 'id',
+                    title: 'ID'
+                },
+                {
+                    data: 'name',
+                    title: 'Name'
+                },
+                {
+                    data: 'sub_total',
+                    title: 'Sub'
+                },
+                {
+                    data: 'total',
+                    title: 'Total'
+                },
+                {
+                    data: 'coupon',
+                    title: 'Coupon'
+                },
+                {
+                    data: 'time',
+                    title: 'Placed @'
+                },
+                {
+                    data: 'action',
+                    title: 'Action'
+                },
             ]
         });
     </script>
