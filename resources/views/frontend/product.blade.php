2a2deef30406659792aef9962850479251b861b3
Mian Umair <mianumair1678@gmail.com>
2021-09-22 13:54:23 +0500

    change whatsapp number

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -126,7 +126,7 @@
                                 @livewire('add-to-cart', ['product_id' => $product->id])
                                 <div class="d-flex justify-content-between pt-2 pb-1">
 
-                                    <a href="{{ 'https://wa.me/+923098456120?text=I am interested in buying *' . $product->name . ' - Rs: ' . ($product->discounted_price ?: $product->original_price) . '*. ' . Request::fullurl() }}"
+                                    <a href="{{ 'https://wa.me/+923414455332?text=I am interested in buying *' . $product->name . ' - Rs: ' . ($product->discounted_price ?: $product->original_price) . '*. ' . Request::fullurl() }}"
                                         target="_blank" class="btn btn-success btn-shadow d-block w-100" type="button">
                                         <i class="fab fa-whatsapp"></i>
                                         &nbsp;Buy via whatsapp
@@ -149,7 +149,7 @@
                                 </div>
                                 <div class="d-flex align-items-center pt-2 pb-4">
 
-                                    <a href="{{ 'https://wa.me/+923098456120?text=When this dress will be back in stock? *' . $product->name . ' - Rs: ' . ($product->discounted_price ?: $product->original_price) . '*. ' . Request::fullUrl() }}"
+                                    <a href="{{ 'https://wa.me/+923414455332?text=When this dress will be back in stock? *' . $product->name . ' - Rs: ' . ($product->discounted_price ?: $product->original_price) . '*. ' . Request::fullUrl() }}"
                                         target="_blank" class="btn btn-success btn-shadow d-block w-100" type="button">
                                         <i class="fab fa-whatsapp"></i>
                                         &nbsp;Inform When Available ?
