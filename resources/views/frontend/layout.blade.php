fbf9e5daecaf3bc893be1d0e052d701a4d7f6f41
Mian Umair <mianumair1678@gmail.com>
2021-09-27 13:25:21 +0500

    open contact us on new tab

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -96,7 +96,7 @@
     </div>
     <!-- Back To Top Button-->
     <a class="btn btn-success rounded-pill btn-icon" style="position: fixed; bottom: 10%; right: 5%; z-index: 100;"
-        href="https://wa.me/+923414455332" id="contactus-button">
+        href="https://wa.me/+923414455332" id="contactus-button" target="_blank">
         <img src="https://img.icons8.com/ios/24/000000/whatsapp--v2.png" /><span style="font-size: 18px;">&nbsp;CONTACT
             US</span>
     </a>
