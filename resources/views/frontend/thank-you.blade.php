3474723544e51ecc55df63ff8496a6e36c0f7fe4
Mian Umair <mianumair1678@gmail.com>
2021-09-27 13:05:05 +0500

    add purchase event

--- resources/views/frontend/thank-you.blade.php
+++ resources/views/frontend/thank-you.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layout')
 
 @section('title')
-Thank you | Danoori
+    Thank you | Danoori
 @endsection
 
 @section('content')
@@ -34,39 +34,24 @@ Thank you | Danoori
 
     <div class="container pb-5 mb-sm-4">
         <div class="pt-5">
-          <div class="card py-3 mt-sm-3">
-            <div class="card-body text-center">
-              <h2 class="h4 pb-3">Thank you for your order!</h2>
-              <p class="fs-sm mb-2">Your order has been placed and will be processed as soon as possible.</p>
-              <p class="fs-sm mb-2">Make sure you make note of your order number, which is <span class="fw-medium">{{ $order_id }}.</span></p>
-              <p class="fs-sm">You will be receiving an email shortly with confirmation of your order. <u>You can now:</u></p><a class="btn btn-secondary mt-3 me-3" href="/">Go back shopping</a> @auth <a class="btn btn-primary mt-3" href="{{ route('dashboard') }}"><i class="ci-location"></i>&nbsp;Track order</a> @endauth
+            <div class="card py-3 mt-sm-3">
+                <div class="card-body text-center">
+                    <h2 class="h4 pb-3">Thank you for your order!</h2>
+                    <p class="fs-sm mb-2">Your order has been placed and will be processed as soon as possible.</p>
+                    <p class="fs-sm mb-2">Make sure you make note of your order number, which is <span
+                            class="fw-medium">{{ $order_id }}.</span></p>
+                    <p class="fs-sm">You will be receiving an email shortly with confirmation of your order. <u>You
+                            can now:</u></p><a class="btn btn-secondary mt-3 me-3" href="/">Go back shopping</a> @auth <a
+                            class="btn btn-primary mt-3" href="{{ route('dashboard') }}"><i
+                            class="ci-location"></i>&nbsp;Track order</a> @endauth
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-
-
+    </div>
 @endsection
 
 @section('extra-js')
     <script>
-        function yesnoCheck() {
-
-            // alert(document.getElementById('customSwitch1').value);
-            if (document.getElementById("customSwitch1").checked) {
-                let password = `<div class="col-sm-6" id="passwordField">
-                                                                                                <div class="mb-3">
-                                                                                                    <label for="password" class="form-label">Password</label>
-                                                                                                    <input name="password" required class="form-control" type="password" id="password">
-                                                                                                </div>
-                                                                                            </div>`;
-                document.getElementById('showPassword').innerHTML += password;
-                document.getElementById('showPassword').style.display = 'block';
-            } else {
-                document.getElementById('passwordField').remove();
-            }
-        }
-
+        fbq('track', 'Purchase');
     </script>
-
 @endsection
