18f1cc30f6960016b6fc15ae71f6589fe4ba6c4f
Mian Umair <mianumair1678@gmail.com>
2021-09-30 13:14:59 +0500

    add pkr as currency

--- resources/views/frontend/thank-you.blade.php
+++ resources/views/frontend/thank-you.blade.php
@@ -54,7 +54,7 @@
     <script>
         let total = {{ Session::get('total') }};
         if (total) {
-            fbq('track', 'Purchase', {currency: 'USD', value: total});
+            fbq('track', 'Purchase', {currency: 'PKR', value: total});
         }
     </script>
 @endsection
