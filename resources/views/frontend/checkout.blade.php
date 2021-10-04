bce3af53d25ea75cdfaac800696f2c69a1dae8c8
Mian Umair <mianumair1678@gmail.com>
2021-10-04 12:17:09 +0500

    change checkout button to cart

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -192,10 +192,10 @@
                     <div class="col-lg-8">
                         <div class="d-flex pt-4 mt-3">
                             <div class="w-50 pe-3">
-                                <a class="btn btn-secondary d-block w-100" href="/">
+                                <a class="btn btn-secondary d-block w-100" href="{{ route('cart')}}">
                                     <i class="ci-arrow-left mt-sm-0 me-1"></i>
-                                    <span class="d-none d-sm-inline">Shop More</span>
-                                    <span class="d-inline d-sm-none">Shop More</span>
+                                    <span class="d-none d-sm-inline">Cart</span>
+                                    <span class="d-inline d-sm-none">Cart</span>
                                 </a>
                             </div>
                             <div class="w-50 ps-2">
@@ -215,8 +215,8 @@
                     <div class="w-50 pe-3">
                         <a class="btn btn-secondary d-block w-100" href="{{ route('cart') }}">
                             <i class="ci-arrow-left mt-sm-0 me-1"></i>
-                            <span class="d-none d-sm-inline">Continue Shopping</span>
-                            <span class="d-inline d-sm-none">Continue Shopping</span>
+                            <span class="d-none d-sm-inline">Back To Cart</span>
+                            <span class="d-inline d-sm-none">Back To Cart</span>
                         </a>
                     </div>
                     <div class="w-50 ps-2">
