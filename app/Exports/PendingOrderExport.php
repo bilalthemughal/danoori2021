a87935dc0a31dc1090b7698647ecda48f5b32647
Mian Umair <mianumair1678@gmail.com>
2021-10-07 16:17:21 +0500

    change weight in excel export

--- app/Exports/PendingOrderExport.php
+++ app/Exports/PendingOrderExport.php
@@ -32,6 +32,6 @@ class PendingOrderExport implements FromCollection,WithHeadings,ShouldAutoSize
     */
     public function collection()
     {
-        return Order::where('status', Order::IS_PENDING)->select(['name','address','phone_number','email','city',DB::raw('"1" as Pieces') , DB::raw('"0.6" as Weight'),'total',DB::raw('order_id as CustomerReferenceNumber'),DB::raw('"No" as SpecialHandling'),DB::raw('"Overnight" as ServiceType'),DB::raw('"clothes" as ProductDetails'),'order_note'])->get();
+        return Order::where('status', Order::IS_PENDING)->select(['name','address','phone_number','email','city',DB::raw('"1" as Pieces') , DB::raw('"0.49" as Weight'),'total',DB::raw('order_id as CustomerReferenceNumber'),DB::raw('"No" as SpecialHandling'),DB::raw('"Overnight" as ServiceType'),DB::raw('"clothes" as ProductDetails'),'order_note'])->get();
     }
 }
