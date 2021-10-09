b08b65e261e37a1727aacab6550ddd212ca5219b
Mian Umair <mianumair1678@gmail.com>
2021-10-09 15:41:16 +0500

    remove purchase event on back button

--- resources/views/frontend/thank-you.blade.php
+++ resources/views/frontend/thank-you.blade.php
@@ -39,20 +39,35 @@
                     <h2 class="h4 pb-3">Thank you for your order!</h2>
                     <p class="fs-sm mb-2">Your order has been placed and will be processed as soon as possible.</p>
                     <p class="fs-sm">We will call you shortly or confirm your order via Whatsapp.</p>
-                    <a class="btn btn-success mt-3 me-3" href="{{ 'https://wa.me/+923414455332?text=Confirm my order: ' . $order_id  }}"><i class="ci-message"></i>&nbsp;&nbsp;Confirm via Whatsapp</a> @auth 
-                            <a class="btn btn-primary mt-3" href="{{ route('dashboard') }}"><i
+                    <a class="btn btn-success mt-3 me-3"
+                        href="{{ 'https://wa.me/+923414455332?text=Confirm my order: ' . $order_id }}"><i
+                            class="ci-message"></i>&nbsp;&nbsp;Confirm via Whatsapp</a> @auth
+                        <a class="btn btn-primary mt-3" href="{{ route('dashboard') }}"><i
                             class="ci-location"></i>&nbsp;Track order</a> @endauth
                 </div>
             </div>
         </div>
     </div>
+
+    <input type="hidden" value="{{ Session::get('total') }}" id="session_total">
 @endsection
 
 @section('extra-js')
     <script>
-        let total = {{ Session::get('total') }};
-        if (total) {
-            fbq('track', 'Purchase', {currency: 'PKR', value: total});
-        }
+        window.addEventListener("pageshow", function(event) {
+            var historyTraversal = event.persisted ||
+                (typeof window.performance != "undefined" &&
+                    window.performance.navigation.type === 2);
+            if (!historyTraversal) {
+                let total = document.querySelector('#session_total').value;
+                if (total) {
+                    fbq('track', 'Purchase', {
+                        currency: 'PKR',
+                        value: total
+                    });
+                    document.querySelector('#session_total').value = '';
+                }
+            }
+        });
     </script>
 @endsection
